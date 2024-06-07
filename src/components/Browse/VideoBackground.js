9c3bd32942da526ef56303030de0d791b7cec14c
unknown <hankishbawa17@gmail.com>
2024-06-08 00:37:34 +0530

    changes saved

--- src/components/Browse/VideoBackground.js
+++ src/components/Browse/VideoBackground.js
@@ -1,32 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { UseMovieTrailer } from '../../utils/customHooks/useMovieTrailer';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { UseMovieTrailer } from "../../utils/customHooks/useMovieTrailer";
+import { useSelector } from "react-redux";
 
 const VideoBackground = ({ movie_id }) => {
-    const [movie_trailer, setMovieTrailer] = useState();
-    const current_movie_trailer = useSelector((store) => { return store.MOVIE_TRAILER; });
-    UseMovieTrailer(movie_id);
+  const [movie_trailer, setMovieTrailer] = useState();
+  const current_movie_trailer = useSelector((store) => {
+    return store.MOVIE_TRAILER;
+  });
+  UseMovieTrailer(movie_id);
 
-    useEffect(() => {
-        if (current_movie_trailer?.isSuccess) {
-            const filtered_movies_trailer = current_movie_trailer?.data?.results?.filter((video) => video?.type === "Trailer");
-            const trailer = filtered_movies_trailer?.length !== 0 ? filtered_movies_trailer[0] : current_movie_trailer?.data?.results[0];
-            setMovieTrailer(trailer);
-        }
-    }, [current_movie_trailer]);
+  useEffect(() => {
+    if (current_movie_trailer?.isSuccess) {
+      const filtered_movies_trailer =
+        current_movie_trailer?.data?.results?.filter(
+          (video) => video?.type === "Trailer"
+        );
+      const trailer =
+        filtered_movies_trailer?.length !== 0
+          ? filtered_movies_trailer[0]
+          : current_movie_trailer?.data?.results[0];
+      setMovieTrailer(trailer);
+    }
+  }, [current_movie_trailer]);
 
-    return (
-        <>
-            <div>
-                <iframe
-                    src={`https://www.youtube.com/embed/${movie_trailer?.key}`}
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin">
-                </iframe>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="w-screen">
+        <iframe
+          className="w-screen aspect-video"
+          src={`https://www.youtube.com/embed/${movie_trailer?.key}?autoplay=1&mute=1`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+        ></iframe>
+      </div>
+    </>
+  );
 };
 
 export default VideoBackground;
--- src/components/Browse/VideoTitle.js
+++ src/components/Browse/VideoTitle.js
@@ -3,11 +3,11 @@ import React from 'react'
 const VideoTitle = ({ title, overview }) => {
     return (
         <>
-            <div className='pt-36 px-12'>
+            <div className='w-screen aspect-video pt-[25%] px-24 absolute text-white bg-gradient-to-r from-black'>
                 <h1 className='text-3xl font-bold'>{title}</h1>
                 <p className='py-6 text-lg w-1/4'>{overview}</p>
                 <div>
-                    <button className='p-2 px-5 bg-gray-400 text-white text-lg rounded-md'>Play</button>
+                    <button className='p-2 px-5 bg-white text-black text-lg rounded-md hover:bg-opacity-50'>Play</button>
                     <button className='mx-4 p-2 px-5 bg-gray-400 text-white text-lg rounded-md'>More Info</button>
                 </div>
             </div>
